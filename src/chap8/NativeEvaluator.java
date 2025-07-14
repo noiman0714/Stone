ea2ae271ae78e05cedcf4de8ff9cabd317c7ca60
Yuuki <noiman0714@gmail.com>
2025-07-14 10:25:43 +0900

    add chap8 program

---
+++ src/chap8/NativeEvaluator.java
@@ -0,0 +1,32 @@
+package chap8;
+import java.util.List;
+
+import chap6.BasicEvaluator.ASTreeEx;
+import chap6.Environment;
+import chap7.FuncEvaluator;
+import javassist.gluonj.Require;
+import javassist.gluonj.Reviser;
+import stone.StoneException;
+import stone.ast.ASTree;
+
+@Require(FuncEvaluator.class)
+@Reviser public class NativeEvaluator {
+	@Reviser public static class NativeArgEx extends FuncEvaluator.ArgumentsEx{
+		public NativeArgEx(List<ASTree> c) {super(c);}
+		@Override public Object eval(Environment callerEnv,Object value) {
+			if(!(value instanceof NativeFunction))
+				return super.eval(callerEnv, value);
+			NativeFunction func = (NativeFunction)value;
+			int nparams = func.numOfParameters();
+			if (size() != nparams)
+                throw new StoneException("bad number of arguments", this);
+            Object[] args = new Object[nparams];
+            int num = 0;
+            for (ASTree a: this) {
+                ASTreeEx ae = (ASTreeEx)a;
+                args[num++] = ae.eval(callerEnv);
+            }
+            return func.invoke(args, this);
+		}
+	}
+}
